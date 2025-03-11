8478a9b31d17111927c4130e170523af09c462c8
liliene <108309670+liliene@users.noreply.github.com>
2025-03-11 12:08:48 -0300

    Update login.js
    Concertando pequenos detalhes.

--- views/usuario/login.js
+++ views/usuario/login.js
@@ -1,5 +1,5 @@
-document.querySelector('form').addEventListener('submit', async (e) => {
-    e.preventDefault();
+document.querySelector('form').addEventListener('submit', async (event) => {
+    event.preventDefault();
 
     const email = document.getElementById('email').value;
     const senha = document.getElementById('senha').value;
@@ -7,10 +7,15 @@ document.querySelector('form').addEventListener('submit', async (e) => {
     const response = await fetch('http://localhost:3000/usuarios/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ email, senha }),
+        body: JSON.stringify({ email, senha })
     });
 
+    const result = await response.json();
     if (response.ok) {
-        window.location.href = './solicitar-manutencao.html';
+        alert('Login bem-sucedido!');
+        localStorage.setItem('usuario', JSON.stringify(result.usuario));
+        window.location.href = './perfil.html';
+    } else {
+        alert(result.message);
     }
 });
