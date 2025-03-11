b1b1ac4d2085a28ccb69ca09dbbf495ef964a165
liliene <108309670+liliene@users.noreply.github.com>
2025-03-11 12:09:16 -0300

    Update solicitar-manutencao.js
    Mudando os nomes de algumas funções.

--- views/usuario/solicitar-manutencao.js
+++ views/usuario/solicitar-manutencao.js
@@ -1,18 +1,19 @@
 document.querySelector('form').addEventListener('submit', async (e) => {
     e.preventDefault();
 
+    const usuario = JSON.parse(localStorage.getItem('idusuario'));
     const bloco = document.getElementById('bloco').value;
     const sala = document.getElementById('sala').value;
     const aparelho = document.getElementById('aparelho').value;
     const problema = document.getElementById('problema').value;
     const descricao = document.getElementById('descrica0').value;
 
-    const response = await fetch('http://localhost:3000/solicitacoes/criar', {
-        method: 'POST', 
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ bloco, sala, aparelho, problema, descricao }),
+    const response = await fetch('http://localhost:3000/manutencao/solicitar', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({usuario_id: usuario.id, bloco, sala, aparelho, problema, descricao})
     });
 
-    const result = await response.json();
-    alert(result.message);
+    const data = await response.json();
+    alert(data.message || data.error);
 });
